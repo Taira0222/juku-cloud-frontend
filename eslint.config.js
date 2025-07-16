ae6c4c0519cdde23534d026da2d9c276b8946da7
Taira Aikawa <tir.3t.f22@gmail.com>
2025-07-16 05:46:41 +0000

    npm run  lint が正常になるようにeslint.config.js を修正した

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,31 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 import pluginReact from 'eslint-plugin-react';
-import storybook from 'eslint-plugin-storybook';
+import { defineConfig } from 'eslint/config';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig([
   {
-    files: ['**/*.{ts,tsx,js,jsx}'],
-    languageOptions: {
-      globals: globals.browser,
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
-    files: ['**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-    plugins: { storybook },
-    extends: ['plugin:storybook/recommended'],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+    },
   },
 ]);
