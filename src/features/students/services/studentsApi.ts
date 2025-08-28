0707469c5aad4f8c13e6ed41bd54601a8fa9b9ef
Taira Aikawa <tir.3t.f22@gmail.com>
2025-08-28 20:09:36 +0000

    不要なconsole.log を削除

--- src/features/students/services/studentsApi.ts
+++ src/features/students/services/studentsApi.ts
@@ -3,6 +3,5 @@ import type { fetchStudentsResponse } from '../types/students';
 
 export const fetchStudents = async (): Promise<fetchStudentsResponse> => {
   const response = await api.get<fetchStudentsResponse>('/students');
-  console.log(response.data);
   return response.data;
 };
