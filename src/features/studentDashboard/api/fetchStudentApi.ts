d207ddbf39db25446255989e683dc4d7d5dc623d
Taira Aikawa <tir.3t.f22@gmail.com>
2025-09-29 07:20:44 +0000

    不要なconsole.logを削除

--- src/features/studentDashboard/api/fetchStudentApi.ts
+++ src/features/studentDashboard/api/fetchStudentApi.ts
@@ -3,6 +3,5 @@ import { StudentDetailSchema } from "../type/studentDashboard";
 
 export const fetchStudent = async (studentId: number) => {
   const { data } = await api.get(`/dashboards/${studentId}`);
-  console.log(data);
   return StudentDetailSchema.parse(data);
 };
